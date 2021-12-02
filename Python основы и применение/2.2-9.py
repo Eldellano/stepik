3d5210c72521dc70031f81abb8a43139c7f576e9
Vladislav Kalman <Vladislav_99355@gmail.com>
2021-12-03 00:41:32 +0300

    add code

---
+++ Python основы и применение/2.2-9.py
@@ -0,0 +1,27 @@
+import simplecrypt
+
+
+password = []
+
+with open('encrypted.bin', 'rb') as inp:
+    encrypted = inp.read()
+
+inf = open('passwords.txt', 'r')
+for i in inf.readlines():
+    password.append(i.strip())
+inf.close()
+
+
+def decription():
+    cnt = 0
+    for i in range(len(password)):
+        print(cnt, i)  # Для визуализации процесса перебора
+        try:
+            if simplecrypt.decrypt(password[cnt], encrypted):
+                print(simplecrypt.decrypt(password[cnt], encrypted).decode('utf8'))
+                cnt += 1
+        except:
+            cnt += 1
+
+
+decription()
