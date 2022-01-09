0b4244c718cfa78f7054d83138b12aaaf2b13df7
Vladislav Kalman <Vladislav_993@mail.ru>
2022-01-09 04:21:57 +0300

    add code

--- Python основы и применение/3.4-2.py
+++ Python основы и применение/3.4-2.py
@@ -6,7 +6,7 @@ date = r'(.{6}2015.+)'
 crime_type = []
 with open('Crimes.csv') as file:
     reader = csv.reader(file)
-    for i in reader:  # Перебираем строки, выбираем нужную даду, добавляем преступление в список
+    for i in reader:  # Перебираем строки, выбираем нужную дату, добавляем преступление в список
         match = re.findall(date, i[2])
         if len(match) > 0:
             crime_type.append(i[5])
