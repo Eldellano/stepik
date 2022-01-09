4395e036418884d5e82bd282749b3ffffeb02809
Vladislav Kalman <Vladislav_993@mail.ru>
2022-01-09 04:21:31 +0300

    add code

--- Python основы и применение/3.4-2.py
+++ Python основы и применение/3.4-2.py
@@ -6,12 +6,12 @@ date = r'(.{6}2015.+)'
 crime_type = []
 with open('Crimes.csv') as file:
     reader = csv.reader(file)
-    for i in reader:
+    for i in reader:  # Перебираем строки, выбираем нужную даду, добавляем преступление в список
         match = re.findall(date, i[2])
         if len(match) > 0:
             crime_type.append(i[5])
 
-cnt = Counter(crime_type)
-for key, values in cnt.items():
+cnt = Counter(crime_type)  # Получаем число повторений
+for key, values in cnt.items():  # Перебираем словарь, выводим ключ по максимальному значению повторений
     if values == max(cnt.values()):
         print(key)
